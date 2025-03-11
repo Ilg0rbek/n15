171b09fff9aea1cd4de26ad3f8cd14b0c6d84751
Ilg0rbek <ilgorbekabdumalikov331@gmail.com>
2025-03-11 10:10:14 +0500

    done

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hammasi ok!';
+    return 'Hammasi sizlar uchun n15 😂';
   }
 }
